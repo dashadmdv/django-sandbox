f0daf4756cb0e1effeff3b549df8546ac60202a0
dashadmdv <apushkins7@gmail.com>
2024-06-10 22:23:18 +0300

    add: index and categories pages

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Страница приложения women")
+
+
+def categories(request):
+    return HttpResponse("<h1>Статьи по категориям</h1>")
+
 
 # Create your views here.
