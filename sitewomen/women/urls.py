1735fb7755a39f9a5da58aa0c262f66da0add4bc
dashadmdv <apushkins7@gmail.com>
2024-06-11 13:11:41 +0300

    add: path converters (int, slug, regexp, custom) (#6)

--- sitewomen/women/urls.py
+++ sitewomen/women/urls.py
@@ -1,5 +1,13 @@
-from django.urls import path
+from django.urls import path, re_path, register_converter
 
-from . import views
+from . import views, converters
 
-urlpatterns = [path("", views.index), path("cats/", views.categories)]
+register_converter(converters.FourDigitYearConverter, "yyyy")
+
+urlpatterns = [
+    path("", views.index),
+    path("cats/<int:cat_id>/", views.categories),
+    path("cats/<slug:cat_slug>/", views.categories_by_slug),
+    re_path(r"^archive_reg/(?P<year>[0-9]{4})/", views.archive),
+    path("archive/<yyyy:year>/", views.archive),
+]
--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -5,8 +5,13 @@ def index(request):
     return HttpResponse("Страница приложения women")
 
 
-def categories(request):
-    return HttpResponse("<h1>Статьи по категориям</h1>")
+def categories(request, cat_id):
+    return HttpResponse(f"<h1>Статьи по категориям</h1><p>id: {cat_id}</p>")
 
 
-# Create your views here.
+def categories_by_slug(request, cat_slug):
+    return HttpResponse(f"<h1>Статьи по категориям</h1><p>slug: {cat_slug}</p>")
+
+
+def archive(request, year):
+    return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
