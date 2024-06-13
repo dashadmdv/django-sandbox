bbdf625118068322c15b7abd40f168310184508c
dashadmdv <apushkins7@gmail.com>
2024-06-13 20:19:10 +0300

    add: save form data to db (#45)

--- sitewomen/women/forms.py
+++ sitewomen/women/forms.py
@@ -4,9 +4,11 @@ from women.models import Category, Husband
 
 
 class AddPostForm(forms.Form):
-    title = forms.CharField(max_length=255)
-    slug = forms.SlugField(max_length=255)
-    content = forms.CharField(widget=forms.Textarea(), required=False)
-    is_published = forms.BooleanField()
-    cat = forms.ModelChoiceField(queryset=Category.objects.all())
-    husband = forms.ModelChoiceField(queryset=Husband.objects.all(), required=False)
+    title = forms.CharField(max_length=255, label="Title", widget=forms.TextInput(attrs={"class": "form-input"}))
+    slug = forms.SlugField(max_length=255, label="URL")
+    content = forms.CharField(widget=forms.Textarea(attrs={"cols": 50, "rows": 5}), required=False, label="Content")
+    is_published = forms.BooleanField(required=False, initial=True, label="Status")
+    cat = forms.ModelChoiceField(queryset=Category.objects.all(), empty_label="Category not chosen", label="Categories")
+    husband = forms.ModelChoiceField(
+        queryset=Husband.objects.all(), required=False, empty_label="Not married", label="Husband"
+    )
