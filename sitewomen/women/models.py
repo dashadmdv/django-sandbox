7a1802c73f33a5d22b287952c43fb7bc7d0b0747
dashadmdv <apushkins7@gmail.com>
2024-06-15 00:04:13 +0300

    add: restrict access via login_required&LoginRequiredMixin (#63)

--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.core.validators import MinLengthValidator, MaxLengthValidator
 from django.db import models
 from django.urls import reverse
@@ -27,6 +28,9 @@ class Women(models.Model):
     cat = models.ForeignKey("Category", on_delete=models.CASCADE, related_name="posts")
     tags = models.ManyToManyField("TagPost", blank=True, related_name="tags")
     husband = models.OneToOneField("Husband", on_delete=models.SET_NULL, null=True, blank=True, related_name="woman")
+    author = models.ForeignKey(
+        get_user_model(), on_delete=models.SET_NULL, related_name="posts", null=True, default=None
+    )
 
     objects = models.Manager()
     published = PublishedManager()
