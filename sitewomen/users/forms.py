f397a3dac50c5ab4b770a2611b2ccd0a3c5677e4
dashadmdv <apushkins7@gmail.com>
2024-06-15 00:35:23 +0300

    add: simple registration (#64)

--- sitewomen/users/forms.py
+++ sitewomen/users/forms.py
@@ -10,3 +10,30 @@ class LoginUserForm(AuthenticationForm):
     class Meta:
         model = get_user_model()
         fields = ["username", "password"]
+
+
+class RegisterUserForm(forms.ModelForm):
+    username = forms.CharField(label="Login")
+    password = forms.CharField(label="Password", widget=forms.PasswordInput())
+    password2 = forms.CharField(label="Repeat password", widget=forms.PasswordInput())
+
+    class Meta:
+        model = get_user_model()
+        fields = ["username", "email", "first_name", "last_name", "password", "password2"]
+        labels = {
+            "email": "E-mail",
+            "first_name": "First name",
+            "last_name": "Last name",
+        }
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd["password"] != cd["password2"]:
+            raise forms.ValidationError("passwords don't match!")
+        return cd["password"]
+
+    def clean_email(self):
+        email = self.cleaned_data["email"]
+        if get_user_model().objects.filter(email=email).exists():
+            raise forms.ValidationError("This email already exists!")
+        return email
