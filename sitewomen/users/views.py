8e2a4783f510817b698ece038c1382ae34f34c9a
dashadmdv <apushkins7@gmail.com>
2024-06-14 23:43:03 +0300

    add: login/logout via LoginView,LogoutView (#62)

--- sitewomen/users/views.py
+++ sitewomen/users/views.py
@@ -1,23 +1,31 @@
-from django.contrib.auth import authenticate, login, logout
-from django.shortcuts import render, redirect
+from django.contrib.auth.views import LoginView
 
 from users.forms import LoginUserForm
 
 
-def login_user(request):
-    if request.method == "POST":
-        form = LoginUserForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            user = authenticate(request, username=cd["username"], password=cd["password"])
-            if user and user.is_active:
-                login(request, user)
-                return redirect("home")
-    else:
-        form = LoginUserForm()
-    return render(request, "users/login.html", {"form": form})
+class LoginUser(LoginView):
+    form_class = LoginUserForm
+    template_name = "users/login.html"
+    extra_context = {"title": "Авторизация"}
+
+    # def get_success_url(self):
+    #     return reverse_lazy("home")
 
 
-def logout_user(request):
-    logout(request)
-    return redirect("users:login")
+# def login_user(request):
+#     if request.method == "POST":
+#         form = LoginUserForm(request.POST)
+#         if form.is_valid():
+#             cd = form.cleaned_data
+#             user = authenticate(request, username=cd["username"], password=cd["password"])
+#             if user and user.is_active:
+#                 login(request, user)
+#                 return redirect("home")
+#     else:
+#         form = LoginUserForm()
+#     return render(request, "users/login.html", {"form": form})
+
+
+# def logout_user(request):
+#     logout(request)
+#     return redirect("users:login")
