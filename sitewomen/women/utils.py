07ec9b510c2e604b9fc09a91a7c926321c48a312
dashadmdv <apushkins7@gmail.com>
2024-06-14 22:58:03 +0300

    add: header menu via custom simple tag (#61)

--- sitewomen/women/utils.py
+++ sitewomen/women/utils.py
@@ -16,14 +16,10 @@ class DataMixin:
         if self.title_page:
             self.extra_context["title"] = self.title_page
 
-        if "menu" not in self.extra_context:
-            self.extra_context["menu"] = menu
-
         if self.cat_selected is not None:
             self.extra_context["cat_selected"] = self.cat_selected
 
     def get_mixin_context(self, context, **kwargs):
-        context["menu"] = menu
         context["cat_selected"] = None
         context.update(kwargs)
         return context
