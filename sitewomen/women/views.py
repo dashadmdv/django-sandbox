c1eadefdbdf44708ecc9f1d89db94b1c571af4be
dashadmdv <apushkins7@gmail.com>
2024-06-14 13:28:19 +0300

    add: upload files on server (#48)

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render, get_object_or_404, redirect
 
-from women.forms import AddPostForm
+from women.forms import AddPostForm, UploadFileForm
 from women.models import Women, Category, TagPost
 
 menu = [
@@ -24,10 +24,24 @@ def index(request):
     return render(request, "women/index.html", context=data)
 
 
+def handle_uploaded_file(f):
+    with open(f"uploads/{f.name}", "wb+") as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
+
+
 def about(request):
+    if request.method == "POST":
+        # handle_uploaded_file(request.FILES["file_upload"])
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            handle_uploaded_file(form.cleaned_data["file"])
+    else:
+        form = UploadFileForm()
     data = {
         "title": "О сайте",
         "menu": menu,
+        "form": form,
     }
     return render(request, "women/about.html", data)
 
