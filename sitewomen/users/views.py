57eb8cdff4fd3827ec7917e97218b5bbf55ae85a
dashadmdv <apushkins7@gmail.com>
2024-06-15 01:09:05 +0300

    add: email auth & user profile (#66)

--- sitewomen/users/views.py
+++ sitewomen/users/views.py
@@ -1,8 +1,10 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 
-from users.forms import LoginUserForm, RegisterUserForm
+from users.forms import LoginUserForm, RegisterUserForm, ProfileUserForm
 
 
 class LoginUser(LoginView):
@@ -21,14 +23,14 @@ class RegisterUser(CreateView):
     success_url = reverse_lazy("users:login")
 
 
-# def register(request):
-#     if request.method == "POST":
-#         form = RegisterUserForm(request.POST)
-#         if form.is_valid():
-#             user = form.save(commit=False)
-#             user.set_password(form.cleaned_data["password"])
-#             user.save()
-#             return render(request, "users/register_done.html")
-#     else:
-#         form = RegisterUserForm()
-#     return render(request, "users/register.html", {"form": form})
+class ProfileUser(LoginRequiredMixin, UpdateView):
+    model = get_user_model()
+    form_class = ProfileUserForm
+    template_name = "users/profile.html"
+    extra_context = {"title": "Профиль пользователя"}
+
+    def get_success_url(self):
+        return reverse_lazy("users:profile")
+
+    def get_object(self, queryset=None):
+        return self.request.user
