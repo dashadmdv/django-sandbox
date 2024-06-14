dee61b74c6997f5a2e6672cd8ed64a092db2ef1e
dashadmdv <apushkins7@gmail.com>
2024-06-14 14:20:49 +0300

    add: show uploaded files on page (#50)

--- sitewomen/sitewomen/urls.py
+++ sitewomen/sitewomen/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from sitewomen import settings
 from women.views import page_not_found
 
 urlpatterns = [
@@ -25,5 +27,8 @@ urlpatterns = [
     path("", include("women.urls")),
     path("__debug__", include("debug_toolbar.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = page_not_found
