2633c58110020f0815a9980158b875435c235a13
dashadmdv <apushkins7@gmail.com>
2024-06-26 18:10:22 +0300

    add: user permissions&groups

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,5 +1,5 @@
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.core.paginator import Paginator
 from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render, get_object_or_404
@@ -46,11 +46,12 @@ class ShowPost(DataMixin, DetailView):
         return get_object_or_404(Women.published, slug=self.kwargs[self.slug_url_kwarg])
 
 
-class AddPage(LoginRequiredMixin, DataMixin, CreateView):
+class AddPage(PermissionRequiredMixin, LoginRequiredMixin, DataMixin, CreateView):
     model = Women
     fields = "__all__"
     template_name = "women/addpage.html"
     title_page = "Добавление статьи"
+    permission_required = "women.add_women"
 
     def form_valid(self, form):
         w = form.save(commit=False)
@@ -58,12 +59,13 @@ class AddPage(LoginRequiredMixin, DataMixin, CreateView):
         return super().form_valid(form)
 
 
-class UpdatePage(DataMixin, UpdateView):
+class UpdatePage(PermissionRequiredMixin, DataMixin, UpdateView):
     model = Women
     fields = ["title", "content", "photo", "is_published", "cat"]
     template_name = "women/addpage.html"
     success_url = reverse_lazy("home")
     title_page = "Редактирование статьи"
+    permission_required = "women.change_women"
 
 
 class DeletePage(DataMixin, DeleteView):
@@ -73,6 +75,7 @@ class DeletePage(DataMixin, DeleteView):
     title_page = "Удаление статьи"
 
 
+@permission_required(perm="women.view_women", raise_exception=True)
 def contact(request):
     return HttpResponse("Обратная связь")
 
